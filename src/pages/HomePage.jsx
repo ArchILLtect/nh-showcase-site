1e1636465913ec4cc75764db26971d47bfdc4139
ArchILLtect <nickhansonsr@gmail.com>
2025-06-13 18:40:18 -0500

    Update: add support for hash linking

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,6 +31,16 @@ const HomePage = () => {
   }, []);
 
   useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, []);
+
+  useEffect(() => {
     trackVisit();
   }, []);
 
