4cef31000b90544b92b24531b64ebaac0bd694c2
ArchILLtect <nickhansonsr@gmail.com>
2025-09-08 22:23:42 -0500

    Update: add fade-in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      boxShadow: {
-        'dark': '0 4px 6px -1px rgba(255, 255, 255, 0.3), 0 2px 4px -2px rgba(255, 255, 255, 0.3)',
-        'darklg': '0 10px 15px -3px rgba(255, 255, 255, 0.3), 0 4px 6px -4px rgba(255, 255, 255, 0.3)'
-      },
-      backgroundImage: {
-        'hero-pattern': "url('../public/images/Hero-Banner-AI.webp')",
-      }
-    }
-  },
-  plugins: [],
-}
-
+    darkMode: "class",
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            boxShadow: {
+                dark: "0 4px 6px -1px rgba(255, 255, 255, 0.3), 0 2px 4px -2px rgba(255, 255, 255, 0.3)",
+                darklg: "0 10px 15px -3px rgba(255, 255, 255, 0.3), 0 4px 6px -4px rgba(255, 255, 255, 0.3)",
+            },
+            backgroundImage: {
+                "hero-pattern": "url('../public/images/Hero-Banner-AI.webp')",
+            },
+            keyframes: {
+                fadeIn: {
+                    "0%": {
+                        opacity: "0",
+                        transform: "scale(.98) translateY(4px)",
+                    },
+                    "100%": { opacity: "1", transform: "none" },
+                },
+            },
+            animation: {
+                fadeIn: "fadeIn 120ms ease-out both",
+            },
+        },
+    },
+    plugins: [],
+};
