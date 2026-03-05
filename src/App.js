30715518fa105bf1d3e87067b50a9b548d4ce193
ArchILLtect <nickhansonsr@gmail.com>
2026-03-04 18:46:04 -0600

    Update: import React in App.js and index.js for component rendering

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@
  *
  */
 
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import "./App.scss";
