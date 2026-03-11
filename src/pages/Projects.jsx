1bf1df54476fff5c720a1c708b30a635f156c85c
ArchILLtect <nickhansonsr@gmail.com>
2026-03-11 07:30:11 -0500

    feat: replace inline project display with ProjectCard component for improved code organization

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -20,14 +20,13 @@
  * - AppModal: A component to display a modal with a live demo of the app.
  * - trackVisit: A utility function to track visits to the projects page.
  * - LoadingSpinner: A component for display a spinner during loading times.
-
  */
 
 import React, { useEffect, useState } from 'react';
-import AppModal from '../components/AppModal';
 import { trackVisit } from "../utils/visitTracker";
 import LoadingSpinner from '../components/LoadingSpinner';
 import Video from '../components/Video';
+import ProjectCard from '../components/ProjectCard';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
@@ -92,98 +91,10 @@ const Projects = () => {
           <LoadingSpinner />
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {/* TODO: Extract the following as a reusable component "<ProjectCard></ProjectCard>" */}
             {projects.map((project, index) => (
-              project.status?.trim() !== "None" ? (
-                <div key={index} className="bg-gray-200 dark:bg-gray-800 border border-gray-300
-                    rounded-lg shadow-md dark:shadow-dark overflow-hidden hover:scale-110">
-                  <img
-                    src={project.image}
-                    alt={`${project.title} thumbnail`}
-                    className="w-full h-48 object-contain"
-                  />
-                  <div className="p-4">
-                    <h2 className="text-gray-700 dark:text-gray-200 text-xl font-semibold mb-2">
-                      {project.title}
-                    </h2>
-                    <p className="dark:text-gray-300 text-gray-600 mb-4 h-24 overflow-y-auto scrollbar-custom p-0.5">
-                      {project.description}
-                    </p>
-                    <p className="dark:text-gray-300 text-sm text-gray-500 font-semibold mb-4 h-12 overflow-y-auto scrollbar-custom">
-                      Tech Stack: {project.techStack.join(', ')}
-                    </p>
-                    <div className="flex justify-between items-center">
-                      {project.liveDemo && (
-                        <AppModal site={project.liveDemo } /> /* Pass liveDemo as site */
-                      )}
-                      {project.siteLink && (
-                        <div className="flex items-center">
-                          <a
-                            href={project.siteLink}
-                            className="dark:text-green-300 text-green-700 text-2xl hover:underline font-bold"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            Live Site
-                          </a>
-                        </div>
-                      )}
-                      {!project.siteLink && !project.liveDemo && (
-                        <div className="flex items-center">
-                          <p className="dark:text-red-500 text-red-700">
-                            No Live Demo Available Yet
-                          </p>
-                        </div>
-                      )}
-                      {project.github && (
-                        <div className="flex items-center">
-                          <a
-                            href={project.github}
-                            className="dark:text-blue-300 text-blue-700 hover:underline"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            GitHub
-                          </a>
-                        </div>
-                      )}
-                    </div>
-                    {project.status && (
-                      <div className="flex items-center p-2 mt-3 justify-center
-                          bg-yellow-100 dark:bg-yellow-800 rounded">
-                        <p className="dark:text-yellow-300 text-yellow-700 font-semibold">
-                          {project.status}
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              ) : (
-              <div key={index} className="bg-gray-200 dark:bg-gray-800 border border-gray-300
-                  rounded-lg shadow-md dark:shadow-dark overflow-hidden opacity-50">
-                  <img
-                    src={project.image}
-                    alt={`${project.title} thumbnail`}
-                    className="w-full h-48 object-contain"
-                  />
-                <div className="p-4">
-                  <h2 className="text-gray-700 dark:text-gray-200 text-xl font-semibold mb-2">
-                    {project.title}
-                  </h2>
-                  <p className="dark:text-gray-300 text-gray-600 mb-4">
-                    {project.description}
-                  </p>
-                  <p className="dark:text-gray-300 text-sm text-gray-500 mb-4">
-                    Tech Stack: {project.techStack.join(', ')}
-                  </p>
-                  <div className="flex space-x-4">
-                    <p className="text-gray-700 dark:text-red-500">
-                      Project Not Yet Deployed
-                    </p>
-                  </div>
-                </div>
+              <div key={index}>
+                <ProjectCard project={project} />
               </div>
-              )
             ))}
           </div>
         )}
