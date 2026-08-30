78b00ca9e03044e861f9b735bbfcfbf00bc5bd5b
ArchILLtect <nickhansonsr@gmail.com>
2026-08-30 16:45:12 -0500

    chore: clean post-vite tooling residue

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,6 @@ module.exports = {
                 dark: "0 4px 6px -1px rgba(255, 255, 255, 0.3), 0 2px 4px -2px rgba(255, 255, 255, 0.3)",
                 darklg: "0 10px 15px -3px rgba(255, 255, 255, 0.3), 0 4px 6px -4px rgba(255, 255, 255, 0.3)",
             },
-            backgroundImage: {
-                "hero-pattern": "url('../public/images/Hero-Banner.png')",
-            },
             keyframes: {
                 fadeIn: {
                     "0%": {
